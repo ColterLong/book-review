34795fd3030d5b5ba41e7664b41d7eb34f633b00
ColterLong <colterlong@protonmail.com>
2023-03-30 14:35:31 -0500

    make collection if not there

--- pages/bookmarks.js
+++ pages/bookmarks.js
@@ -1,27 +1,50 @@
 import React from 'react'
 import Favorites from '@/components/Favorites'
 import {useState, useEffect} from 'react'
-import { getBooks } from "../utils/books"
+import { getBooks, verifyUserInDatabase } from "../utils/books"
 import BookPreviewFavoritesCollection from '../components/BookPreviewFavoritesCollection'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
 
 
 const bookmarks = () => {
   const [books, setBooks] = useState([]);
+  const [currentUser, setCurrentUser] = useState();
 
+  const auth = getAuth();
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      setCurrentUser(user.uid)
+      console.log(currentUser)
+
+      // let verifyUser = await verifyUserInDatabase(String(currentUser))
+      // console.log(verifyUser)
+      // let books = await getBooks(String(currentUser));
+      // setBooks(books);
+
+    } else {
+      console.log("user not signed in")
+    }
+  })
+
+  
   useEffect(() => {
     const func = async () => {
-      let books = await getBooks();
-      console.log("inside effect: ")
-      console.log(books);
+      // let books = await getBooks("elrtifLALpClRAWjvfeg");
+      let verifyUser = await verifyUserInDatabase(String(currentUser))
+      console.log(verifyUser)
+
+      let books = await getBooks(String(currentUser));
       setBooks(books);
     }
     func()
-  },[])
+  },[currentUser])
+  // also update when user adds/deletes a book
 
-  useEffect(() => {
-    console.log("books was updated")
-    console.log(books)
-  },[books])
+
+  // useEffect(() => {
+  //   console.log("books was updated")
+  //   console.log(books)
+  // },[books])
 
   return (
     <>
--- utils/books.js
+++ utils/books.js
@@ -1,35 +1,59 @@
-import { getFirestore, doc, getDoc, getDocs, collection, query } from 'firebase/firestore'
-import { getAuth, onAuthStateChanged } from 'firebase/auth'
+import { getFirestore, addDoc, setDoc, doc, getDoc, getDocs, collection, query } from 'firebase/firestore'
 
-const auth = getAuth();
-onAuthStateChanged(auth, (user) => {
-  if (user) {
-    console.log("user signed in:")
-    console.log(user);
-    console.log("user uid:")
-    console.log(user.uid)
+const verifyUserInDatabase = async (user) => {
+  const db = getFirestore();
+  const q = doc(db, "users", user);
+  const querySnapshot = await getDoc(q);
+
+  if (querySnapshot.exists()) {
+    console.log("user exists in database")
+  } else if (user === "undefined") {
+    console.log("user is undefined, should update in a minute")
+    // setDoc(q, {merge: true});
   } else {
-    console.log("user not signed in")
+    console.log("user actually doesn't exist")
+    console.log(querySnapshot.exists())
+    console.log(user);
+    // await setDoc(q, { favorites: "favorites" })
+    await setDoc(q, {merge: true})
+
+    // still adds another book to it. probably just add
+    // await addDoc(collection(db, "users", user, "favorites"), {title: "first_book"})
+    
   }
-})
+}
 
-const getBooks = async () => {
+// const verifyUserInDatabase = async (user) => {
+//   const db = getFirestore();
+//   try {
+//     await runTransaction(db, async (transaction) => {
+//       const sfDoc = await transaction.get(sfDocRef);
+//       if (!sfDoc.exists()) {
+//         throw "Document does not exist!";
+//       }
+  
+//       const newPopulation = sfDoc.data().population + 1;
+//       transaction.update(sfDocRef, { population: newPopulation });
+//     });
+//     console.log("Transaction successfully committed!");
+//   } catch (e) {
+//     console.log("Transaction failed: ", e);
+//   }
+// }
 
+const getBooks = async (user) => {
   const arr = [];
   const db = getFirestore();
- 
-  const q = query(collection(db, "users","elrtifLALpClRAWjvfeg", "favorites"));
-  // const q = query(collection(db, "users", user, "favorites"));
+  // const q = query(collection(db, "users","elrtifLALpClRAWjvfeg", "favorites"));
+  const q = query(collection(db, "users", user, "favorites"));
   const querySnapshot = await getDocs(q);
   querySnapshot.forEach((doc) => {
-    console.log("test query")
-    console.log(doc.id, " => ", doc.data())
+    // console.log("test query")
+    // console.log(doc.id, " => ", doc.data())
     arr.push(doc.data())
   })
-
-
 
   return arr;
 }
 
-export { getBooks };
+export { getBooks, verifyUserInDatabase };
