1c3e317d148c8702fab17494844143567db59d9b
ColterLong <colterlong@protonmail.com>
2023-03-31 00:18:06 -0500

    bookmark button doesn't open new page

--- components/BookPreviewCard.js
+++ components/BookPreviewCard.js
@@ -28,14 +28,15 @@ const BookPreviewCard = ( {user, title, author, bookImage, description, isbn10, 
           height={300}
         />
         <div className={styles.container}>
-          <button onClick={() => pushToFavorites(String(user), title, bookImage)}>
-            {/* svg */}
-            <svg width="17" height="20" xmlns="http://www.w3.org/2000/svg"><path d="M15.387 0c.202 0 .396.04.581.119.291.115.522.295.694.542.172.247.258.52.258.82v17.038c0 .3-.086.573-.258.82a1.49 1.49 0 0 1-.694.542 1.49 1.49 0 0 1-.581.106c-.423 0-.79-.141-1.098-.423L8.46 13.959l-5.83 5.605c-.317.29-.682.436-1.097.436-.202 0-.396-.04-.581-.119a1.49 1.49 0 0 1-.694-.542A1.402 1.402 0 0 1 0 18.52V1.481c0-.3.086-.573.258-.82A1.49 1.49 0 0 1 .952.119C1.137.039 1.33 0 1.533 0h13.854Z" fill="#5A698F"/></svg>
-          </button>
+          
           <p className={styles.text}>{title}</p>
           <p className={styles.text}>by {author}</p>
         </div>
       </Link>
+      <button className={styles.button} onClick={() => pushToFavorites(String(user), title, bookImage)}>
+            {/* svg */}
+            <svg width="17" height="20" xmlns="http://www.w3.org/2000/svg"><path d="M15.387 0c.202 0 .396.04.581.119.291.115.522.295.694.542.172.247.258.52.258.82v17.038c0 .3-.086.573-.258.82a1.49 1.49 0 0 1-.694.542 1.49 1.49 0 0 1-.581.106c-.423 0-.79-.141-1.098-.423L8.46 13.959l-5.83 5.605c-.317.29-.682.436-1.097.436-.202 0-.396-.04-.581-.119a1.49 1.49 0 0 1-.694-.542A1.402 1.402 0 0 1 0 18.52V1.481c0-.3.086-.573.258-.82A1.49 1.49 0 0 1 .952.119C1.137.039 1.33 0 1.533 0h13.854Z" fill="#5A698F"/></svg>
+      </button>
       {/* <Image
         className={styles.image}
         alt={title}
