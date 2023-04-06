6d6a83308735088d56f5779077867ff3271b902f
ColterLong <colterlong@protonmail.com>
2023-04-06 18:43:42 -0500

    redid login page

--- components/Login.js
+++ components/Login.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styles from './Login.module.css'
+import Image from 'next/image'
 
 const Searchbar = ( {signIn} ) => {
 
@@ -9,7 +10,11 @@ const Searchbar = ( {signIn} ) => {
 
   return (
     <div className={styles.container}>
-        <button onClick={signIn}>Continue with Google</button>
+        <h1>Login</h1>
+        <button onClick={signIn} className={styles.button}>
+          <Image src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" width={32} height={32}/>
+          <h1 className={styles.text}>Sign in with Google</h1>
+        </button>
     </div>
   )
 }
